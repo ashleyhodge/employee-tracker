9803f301f12f2979124acb9c059cc65c162bdeb1
Ashley Hodge <ashhodge@comcast.net>
2022-03-08 22:51:53 -0500

    connect department id to roles table

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,16 @@
-DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
 
-CREATE TABLE roles (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL
-);
 
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
