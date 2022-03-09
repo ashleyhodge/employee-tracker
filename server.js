41409c506d99b5994ff96a081836bb3945e7c370
Ashley Hodge <ashhodge@comcast.net>
2022-03-09 13:55:20 -0500

    add create and delete options for roles

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the business database.')
 );
-
+// View all departments
 app.get('/api/departments', (req, res) => {
     const sql = `SELECT * FROM departments`;
 
@@ -32,7 +32,7 @@ app.get('/api/departments', (req, res) => {
         });
     });
 });
-
+// Delete a department
 app.delete('/api/department/:id', (req, res) => {
     const sql = `DELETE FROM departments WHERE id = ?`;
     const params = [req.params.id];
@@ -71,7 +71,60 @@ app.post('/api/department', ({ body }, res) => {
         });
     });
 });
+// View all roles 
+app.get('/api/roles', (req, res) => {
+    const sql = `SELECT roles.*, departments.department_name AS department_name 
+                    FROM roles 
+                    LEFT JOIN departments ON roles.department_id = departments.id;`;
 
+    db.query(sql, (err, rows) => {
+        if(err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+app.delete('/api/role/:id', (req, res) => {
+    const sql = `DELETE FROM roles WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.status(400).json({ error: err.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Role not found'
+            });
+        } else {
+            res.json({
+                message: 'Role has been successfully deleted!',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+app.post('/api/role', ({ body }, res) => {
+    const sql = `INSERT INTO roles (title, salary, department_id)
+                   VALUES(?,?,?)`;
+    const params = [body.title, body.salary, body.department_id];
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.status(400).json({ error: err.message });
+            return;
+        } 
+        res.json({
+            message: 'Role successfully created!',
+            data: body
+        });
+    });
+});
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
