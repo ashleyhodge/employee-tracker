70d9a2f7e817e949eefe8dbc20fa922a34ba4f98
Ashley Hodge <ashhodge@comcast.net>
2022-03-08 20:23:18 -0500

    connect server.js to database

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,7 +7,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'business'
+    },
+    console.log('Connected to the business database.')
+);
+
 app.get('/', (req, res) => {
+
+    db.query(`SELECT * FROM departments`, (err, rows) => {
+        console.log(rows);
+    })
     res.json({
         message: 'Hello World'
     });
