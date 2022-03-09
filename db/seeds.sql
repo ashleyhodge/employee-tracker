bffcbfb9f78ff6515031cc2b4ef53bd97144d1bc
Ashley Hodge <ashhodge@comcast.net>
2022-03-08 19:30:52 -0500

    create business database and department table

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO departments (department_name)
+VALUES
+    ('Administration/Operations'),
+    ('Research and Development'),
+    ('Marketing and Sales'),
+    ('Human resources'),
+    ('Customer services'),
+    ('Accounting and Finance');
